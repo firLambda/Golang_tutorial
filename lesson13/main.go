8168b7fe17120d9d5c1c13a6eb17a9afdb5d65ad
firLambda <koki19991612@gmail.com>
2024-11-26 19:34:56 +0900

    add files and dir of tutorials

--- lesson13/main.go
+++ lesson13/main.go
@@ -6,7 +6,31 @@ func Plus(x int, y int) int {
 	return x + y
 }
 
+func Div(x, y int) (int, int) {
+	q := x / y
+	r := x % y
+	return q, r
+}
+
+func Double(price int)(result int){
+	result = price * 2
+	return 
+}
+
+func Noreturn(){
+	fmt.Println("No Return")
+	return
+}
+
 func main() {
 	i := Plus(1, 2)
 	fmt.Println(i)
+
+	i2, i3 := Div(4,3)
+	fmt.Println(i2, i3)
+
+	result := Double(100)
+	fmt.Println(result)
+
+	Noreturn()
 }
